08838d7bff95ad1a00a6c3fe2bee8c37f5be646a
world3376 <77318423+world3376@users.noreply.github.com>
2021-07-01 14:42:22 +0900

    Add files via upload

---
+++ study/src/main/java/kr/co/dao/ReplyDAOImpl.java
@@ -0,0 +1,43 @@
+package kr.co.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import kr.co.vo.ReplyVO;
+
+@Repository
+public class ReplyDAOImpl implements ReplyDAO {
+	
+	@Inject SqlSession sql;
+	
+	//댓글조회
+	@Override
+	public List<ReplyVO> readReply(int bno) throws Exception {
+		return sql.selectList("replyMapper.readReply", bno);
+	}
+	//댓글작성
+	@Override
+	public void writeReply (ReplyVO vo) throws Exception {
+		sql.insert("replyMapper.writeReply", vo);
+	}
+	//댓글수정
+	@Override
+	public void updateReply(ReplyVO vo) throws Exception {
+		sql.update("replyMapper.updateReply",vo);
+	}
+	//댓글삭제
+	@Override
+	public void deleteReply(ReplyVO vo) throws Exception {
+		sql.update("replyMapper.deleteReply",vo);
+	}
+	//선택된 댓글조회
+	@Override
+	public ReplyVO selectReply(int rno) throws Exception {
+		return sql.selectOne("replyMapper.selectReply", rno);
+	}
+
+}
